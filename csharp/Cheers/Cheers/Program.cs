ed9a3899d0cb52017cad9f1ed6ac8184d5f013d4
Kirk Walker Graves <graves.kirk@gmail.com>
2016-02-24 15:19:26 -0800

    completed part 2 of Cheers project

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -10,10 +10,17 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            // Get user's name info and format data
             Console.WriteLine("What's your name?");
             string name = Console.ReadLine();
             string lowerName = name.ToLower();
             string anLetters = "halfnorsemix";
+
+            // Get user's birthday info and format data
+            DateTime userBirthday = GetUserBirthday(name);
+            DateTime today = DateTime.Today;
+            double birthdayDelta = (userBirthday - today).TotalDays;
+
             foreach (char letter in lowerName)
             {
                 if (anLetters.Contains(letter))
@@ -21,12 +28,29 @@ namespace Cheers
                     Console.Write("Give me an " + letter + "!\n");
                 }
                 else
-                {
+                { 
                     Console.Write("Give me a " + letter + "!\n");
                 }
             }
             Console.Write(name.ToUpper() + " is just GRAND!\n");
+            if (birthdayDelta <= 0)
+            {
+                Console.Write("Happy birthday, " + name + "!");
+            }
+            else
+            {
+                Console.Write("Your birthday is " + birthdayDelta + " days away!");
+            }
+           
             Console.ReadKey();            
+        }
+
+        private static DateTime GetUserBirthday(string name)
+        {
+            Console.WriteLine("When's your birthday, " + name + "? (MM/DD)");
+            string userBirthday = Console.ReadLine();
+            DateTime birthday = DateTime.Parse(userBirthday);
+            return birthday;
         }
 
         
